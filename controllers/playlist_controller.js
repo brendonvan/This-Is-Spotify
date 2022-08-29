ed24cf6dede870a1cb3223f7b079de720f373b7f
Kevin Ng <kehfunn@gmail.com>
2022-08-29 15:24:48 -0400

    updated server.js controllers, working on playlist_controller.js routes

--- controllers/playlist_controller.js
+++ controllers/playlist_controller.js
@@ -1,17 +1,68 @@
 // CONFIGURATION
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
+const mongoose = require("mongoose");
 
 // MIDDLEWARE
 router.use(express.json());
-router.use(express.urlencoded({ extended: true}));
+router.use(express.urlencoded({ extended: true }));
 
 // ROUTERS
-router.get("/", (req, res) => {
-    const context = {
-        // INSERT HERE
+
+// NEW ROUTE
+// GET request for new playlist(s)
+router.get("/new", (req, res) => {
+    res.render("playlist.ejs")
+});
+
+// INDEX ROUTE
+// GET request for all playlists in our playlist DB
+router.get("/", async (req, res, next) => {
+    try {
+        console.log("finding playlist")
+        const playlist = await db.Playlist.find({});
+        console.log("found playlist")
+        const context = {playlist: playlist};
+        res.render("playlist.ejs", context);
+
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
     }
-    res.render("home.ejs", context)
-})
+});
+
+// SHOW ROUTE
+// GET request for one playlist
+router.get("/:id", async (req, res, next) => {
+    try {
+        const onePlaylist = await db.Playlist.findById(req.params.id);
+        console.log(onePlaylist);
+        const context = {onePlaylist: onePlaylist};
+        res.render("playlist.ejs", context);
+
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+});
+
+// CREATE ROUTE
+// POST request for adding new playlist(s)
+router.post("/", async (req, res, next) => {
+    try {
+        const createdPlaylist = await db.Playlist.create(req.body);
+        console.log(createdPlaylist);
+        res.redirect("/playlist");
+
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next ();
+    }
+});
+
 
 module.exports = router;
---
+++ models/playlist_model.js
@@ -0,0 +1,11 @@
+const playlist = [
+    {
+        playlistname: "late night",
+        title: "Argentina by Gunna",
+        album: "WUNNA",
+        date: "5/6/2022",
+        duration: "2:29"
+    }
+];
+
+module.exports = playlist;
