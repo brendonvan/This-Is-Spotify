5a25f84579598582cc322854780efe3376c9874d
Kevin Ng <kehfunn@gmail.com>
2022-08-30 12:06:31 -0400

    updated Playlist schema

--- models/Playlist.js
+++ models/Playlist.js
@@ -2,25 +2,54 @@ const mongoose = require("mongoose");
 
 const playlistSchema = new mongoose.Schema({
 
-    playlistname: {
+    name_of_playlist: {
         type: String,
         required: [true, "name of playlist cannot be empty"]
     },
-    title: {
-        type: String,
+    tracks: {
+        type: [{String}], // fix this object later
         required: [true, "name cannot be empty"]
     },
     album: {
         type: String,
         required: [true, "album cannot be empty"]
     },
-    date: {
+    image: {
+        type: String,
+        required: [true, "playlist image cannot be empty"]
+    },
+    created_date: {
         type: String,
         required: [true, "date added cannot be empty"]
     },
-    duration: {
+    duration_of_entire_playlist: {
         type: String,
         required: [true, "duration of song cannot be empty"]
+    },
+    number_of_tracks: {
+        type: Number,
+        required: [true, "number of tracks cannot be empty"]
+    },
+    number_of_users_following: {
+        type: Number
+    },
+    playlist_id: {
+        type: String,
+        required: [true, "playlist id cannot be empty"]
+    },
+    track_ids: {
+        type: String,
+        required: [true, "track ids cannot be empty"]
+    },
+    user_id: {
+        type: String,
+        required: [true, "user id cannot be empty"]
+    },
+    description: {
+        type: String
+    },
+    isAlbum: {
+        type: Boolean
     }
 },
 {
