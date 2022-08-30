e36a8de038cbb5e86a1edf79beb022b412a8ba9f
Kevin Ng <kehfunn@gmail.com>
2022-08-30 15:12:47 -0400

    finished models

---
+++ models/seed_tracks.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        title: "Sunshine",
+        album: "",
+        artists: "Tyga, Jhene Aiko, Pop Smoke",
+        image: "https://picsum.photos/200/300",
+        number_of_times_played: 7,
+        duration: "3:09",
+        tracks_id: "54NBD72JXFXzrodbQBSzWh",
+        playlist_ids: "1rv7FvR3ZuGiVUpVhhOjip"
+    }
+];
