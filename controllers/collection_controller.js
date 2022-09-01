4942772c39b086273b35a6c8d605f7db29bdacd6
brendonvan <brendon@van.digital>
2022-08-31 18:02:13 -0700

    Update Search Page and Main Routes

--- controllers/collection_controller.js
+++ controllers/collection_controller.js
@@ -8,39 +8,82 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 // ROUTERS
-router.get("/", (req, res) => {
-    const context = {
-        // INSERT HERE
+router.get("/", (req, res, next) => {
+    try {
+        const context = {
+            // INSERT HERE
+        }
+        res.render("collection.ejs", context)
+    } catch (error) {
+        console.log(error)
+        req.error = error;
+        return next();
     }
-    res.render("collection.ejs", context)
 })
 
-router.get("/playlist", (req, res) => {
-    const context = {
-        // INSERT HERE
+router.get("/albums", (req, res, next) => {
+    try {
+        const context = {
+            // INSERT HERE
+        }
+        res.render("collection.ejs", context)
+    } catch (error) {
+        console.log(error)
+        req.error = error;
+        return next();
     }
-    res.render("collection.ejs", context)
 })
 
-router.get("/podcast", (req, res) => {
-    const context = {
-        // INSERT HERE
+router.get("/tracks", (req, res, next) => {
+    try {
+        const context = {
+            // INSERT HERE
+        }
+        res.render("collection.ejs", context)
+    } catch (error) {
+        console.log(error)
+        req.error = error;
+        return next();
     }
-    res.render("collection.ejs", context)
 })
 
-router.get("/artist", (req, res) => {
-    const context = {
-        // INSERT HERE
+router.get("/genres", (req, res, next) => {
+    try {
+        const context = {
+            // INSERT HERE
+        }
+        res.render("collection.ejs", context)
+    } catch (error) {
+        console.log(error)
+        req.error = error;
+        return next();
     }
-    res.render("collection.ejs", context)
 })
 
-router.get("/album", (req, res) => {
-    const context = {
-        // INSERT HERE
+router.get("/playlists", (req, res, next) => {
+    try {
+        const context = {
+            // INSERT HERE
+        }
+        res.render("collection.ejs", context)
+    } catch (error) {
+        console.log(error)
+        req.error = error;
+        return next();
     }
-    res.render("collection.ejs", context)
+})
+
+router.get("/liked", (req, res, next) => {
+    try {
+        const context = {
+            // INSERT HERE
+        }
+        res.render("collection.ejs", context)
+    } catch (error) {
+        console.log(error)
+        req.error = error;
+        return next();
+    }
 })
 
 module.exports = router;
