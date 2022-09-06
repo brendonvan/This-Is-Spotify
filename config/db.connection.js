a1ff3e934ae8dc1b662000e9e94c371256b4becc
brendonvan <brendon@van.digital>
2022-09-05 21:26:14 -0700

    Update for Heroku Deployment

--- config/db.connection.js
+++ config/db.connection.js
@@ -5,8 +5,6 @@ const mongoose = require('mongoose');
 //environment loading .env file into process.env object
 require('dotenv').config();
 
-// console.log(process.env.MONGODB_URI)
-
 // get the MongoDB URL from .env file
 const connectionStr = process.env.MONGODB_URI;
 
@@ -24,12 +22,3 @@ mongoose.connection.on('disconnected', () => console.log('MongoDB disconnected  
 
 const db = require("../models/");
 const seedData = require("../models/seed_data/");
-
-// NEEDED FOR SEED DATA POPULATING
-// db.Playlist.insertMany(seedData.seed_playlist, (err, db) => {
-//     if (err) {
-//         console.log(err)
-//     }
-//       console.log("added models data", db)
-//       mongoose.connection.close();
-// });
