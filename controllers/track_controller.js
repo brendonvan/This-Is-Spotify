003734d77cad3407310a3c888cec2c4fc8308d70
brendonvan <brendon@van.digital>
2022-12-02 12:57:11 -0700

    Fix switch to render

--- controllers/track_controller.js
+++ controllers/track_controller.js
@@ -9,7 +9,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 
 let client_id = process.env.CLIENT_ID;
 let client_secret = process.env.CLIENT_SECRET;
-let redirect_uri = "https://pokedex-demo.onrender.com:8080/callback";
+let redirect_uri = "https://this-is-streamify.onrender.com:8080/callback";
 
 const spotifyApi = new SpotifyWebApi({
     client_id: client_id,
